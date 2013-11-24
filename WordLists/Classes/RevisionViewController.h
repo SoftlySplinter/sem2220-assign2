afbd87eea573100d779c99418bb8722903db2835
Alex Brown <alex@alexanderdbrown.com>
2013-11-24 19:13:56 +0000

    Start FR4

--- WordLists/Classes/RevisionViewController.h
+++ WordLists/Classes/RevisionViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RevisionViewController : UIViewController
+@interface RevisionViewController : UIViewController <UIPickerViewDataSource,UIPickerViewDelegate>
 
 @end
--- WordLists/Classes/RevisionViewController.m
+++ WordLists/Classes/RevisionViewController.m
@@ -8,6 +8,29 @@
 
 #import "RevisionViewController.h"
 
+@interface RevisionViewController()
+@property (weak, nonatomic) IBOutlet UIPickerView *answerSelection;
+
+@end
+
 @implementation RevisionViewController
+
+-(NSInteger) numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+-(NSInteger) pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return 4;
+}
+
+-(NSString *) pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return [NSString stringWithFormat:@"Row: %d", row];
+}
+
+-(void) pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    
+}
+
+
 
 @end
