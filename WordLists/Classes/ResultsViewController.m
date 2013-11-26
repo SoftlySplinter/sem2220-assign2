b567ae8cb6d9f216d7d1da2fff2457240ba3798f
Alex Brown <alex@alexanderdbrown.com>
2013-11-26 16:22:36 +0000

    Add scoring
    This pretty much completes FR4

---
+++ WordLists/Classes/ResultsViewController.m
@@ -0,0 +1,46 @@
+//
+//  ResultsViewController.m
+//  WordLists
+//
+//  Created by SAW course on 26/11/2013.
+//  Copyright (c) 2013 Aberystwyth University. All rights reserved.
+//
+
+#import "ResultsViewController.h"
+
+@interface ResultsViewController()
+@property (weak, nonatomic) IBOutlet UILabel *scoreField;
+@property (weak, nonatomic) IBOutlet UILabel *congratsField;
+
+@end
+
+@implementation ResultsViewController
+
+-(void) viewDidLoad {
+    self.scoreField.text = [NSString stringWithFormat:@"%d", self.score];
+    switch(self.score) {
+        case 10:
+            self.congratsField.text = @"You are a master of Welsh";
+            break;
+        case 9:
+        case 8:
+        case 7:
+            self.congratsField.text = @"Your Welsh is solid, but could be improved";
+            break;
+        case 6:
+        case 5:
+        case 4:
+            self.congratsField.text = @"You are trying hard, but still have lots of room from improvement";
+            break;
+        case 3:
+        case 2:
+        case 1:
+            self.congratsField.text = @"You can do better than that";
+            break;
+        case 0:
+        default:
+            self.congratsField.text = @"Stick to English";
+    }
+}
+
+@end
--- WordLists/Classes/RevisionViewController.m
+++ WordLists/Classes/RevisionViewController.m
@@ -8,8 +8,11 @@
 
 #import "RevisionViewController.h"
 #import "SharedData.h"
+#import "ResultsViewController.h"
 
 #define QUESTIONS 10
+#define ATTEMPTS 2
+#define CHOICES 4
 
 @interface RevisionViewController()
 @property (weak, nonatomic) IBOutlet UIPickerView *answerSelection;
@@ -22,47 +25,69 @@
 @property (strong, nonatomic) NSMutableArray *choices;
 @property NSInteger questionNumber;
 @property NSInteger correct;
+@property NSInteger attempts;
+@property BOOL shouldSegue;
 
 @end
 
 @implementation RevisionViewController
 
 -(void) viewDidLoad {
-    self.choices = [[NSMutableArray alloc] initWithCapacity:4];
+    self.choices = [[NSMutableArray alloc] initWithCapacity:CHOICES];
 }
 
 -(void) viewDidAppear:(BOOL)animated {
     self.questionNumber = 0;
     self.correct = 0;
+    self.shouldSegue = NO;
     [self.finalConfirmButton setHidden:YES];
     [self.confirmButton setHidden:NO];
     [self nextQuestion];
 }
 
-- (IBAction)doConfirm:(id)sender {
-    NSLog(@"This is where logic would happen to check if the answer is right");
-    
-    if(YES) {
-        [self nextQuestion];
+-(BOOL) answerCorrect {
+    return [self.answerSelection selectedRowInComponent:0] == self.answer;
+}
+
+-(BOOL) shouldContinueAnyway {
+    if(self.attempts < ATTEMPTS - 1) {
+        self.attempts++;
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Invalid Answer" message:[NSString stringWithFormat:@"Invalid Answer, try again (%d attempts remaining)", ATTEMPTS - self.attempts] delegate:Nil cancelButtonTitle:@"OK" otherButtonTitles:Nil];
+        [alert show];
+        return NO;
     } else {
-        
+        WordPair *answer = self.choices[self.answer];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Invalid Answer" message:[NSString stringWithFormat:@"Incorrect Awnser, the correct answer was: %@", answer.english] delegate:Nil cancelButtonTitle:@"OK" otherButtonTitles:Nil];
+        [alert show];
+        return YES;
+    }
+}
+
+- (IBAction)doConfirm:(id)sender {
+    if([self answerCorrect] || [self shouldContinueAnyway]) {
+        if(self.attempts == 0) self.correct++;
+        [self nextQuestion];
     }
 }
 
 - (IBAction)doFinalConfirm:(id)sender {
-    NSLog(@"This is where logic would happen to check if the answer is right");
-    [self nextQuestion];
+    if([self answerCorrect] || [self shouldContinueAnyway]) {
+        if(self.attempts == 0) self.correct++;
+        self.shouldSegue = YES;
+    }
 }
 
 -(void) nextQuestion {
     NSLog(@"Generating next question");
-    if(self.questionNumber == QUESTIONS) {
+    if(self.questionNumber == QUESTIONS - 1) {
         [self.finalConfirmButton setHidden:NO];
         [self.confirmButton setHidden:YES];
     }
     
+    self.attempts = 0;
     self.progress.progress = ((float) self.questionNumber / (float) QUESTIONS);
     [self generateQuestion];
+    [self.answerSelection selectRow:0 inComponent:0 animated:NO];
     self.questionNumber++;
 }
 
@@ -71,7 +96,7 @@
     NSInteger noWords = [[SharedData defaultInstance] numberOfWordsForLanguage:WLLanguageSettingWelsh];
     
     
-    while([self.choices count] < 4) {
+    while([self.choices count] < CHOICES) {
         NSInteger answerIndex = random() % noWords;
         WordLink *answerWord = [[SharedData defaultInstance] wordPairForIndexPosition:answerIndex language:WLLanguageSettingWelsh];
         WordPair *answerPair = [self selectAnswerFromLink: answerWord];
@@ -87,7 +112,7 @@
     
     NSLog(@"%@", self.choices);
     
-    self.answer = random() % 4;
+    self.answer = random() % [self.choices count];
     WordPair *answerPair = self.choices[self.answer];
     self.anserLabel.text = [answerPair welshWithContext];
     
@@ -117,7 +142,17 @@
     
 }
 
+-(void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    ResultsViewController *results = segue.destinationViewController;
+    results.score = self.correct;
+}
 
+-(BOOL) shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    if(sender == self.finalConfirmButton) {
+        return self.shouldSegue;
+    }
+    return YES;
+}
 
 
 
